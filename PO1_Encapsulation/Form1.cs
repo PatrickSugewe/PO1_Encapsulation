35a5215f22f25168413f54e96185eddec22d2af5
PatrickSugewe <157501471+PatrickSugewe@users.noreply.github.com>
2024-02-08 14:28:07 -0500

    P01 AGagnon

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,19 +2,47 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage();
+        Personnage dragon = new();
         public Form1()
         {
             InitializeComponent();
+
+            joueur.PointsDevie = 10;
+            dragon.PointsDevie = joueur.PointsDevie * 3;
+            joueur.PointDePuissance = 1;
+            dragon.PointDePuissance = joueur.PointsDevie * 3;
+           
         }
 
+        private void Attaquer()
+        {
+            {
+                lbl_pointsVieJoueur.Text= " Point de vie :" +
+                    joueur.PointsDevie.ToString();
+
+                lbl_pointsVieDragon.Text= " Point de vie :" +
+                    dragon.PointsDevie.ToString();
+
+                lbl_puissanceAttaqueJoueur.Text = "Points de vie : " +
+                    joueur.PointDePuissance.ToString();
+
+                lbl_puissanceAttaqueDragon.Text = "Point de vie : " +
+                    dragon.PointDePuissance.ToString();
+
+            }
+        }
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointsDevie -= joueur.PointDePuissance;
+            TourDragon();
+            Actualiserlabel();
         }
+       
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    internal class Personnage
+    {
+        private string nom;
+        private int pointsDevie;
+        private int pointDePuissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDevie 
+        {
+            get => pointsDevie;
+            set
+            {
+             
+                if (value > 0)
+                {
+                    pointsDevie = value;
+                }
+                else
+                {
+                    pointsDevie = 0;
+                }
+
+            }   
+        }
+        public int PointDePuissance { get => pointDePuissance; set => pointDePuissance=value; }
+
+        public bool EstEnVie
+        {
+            get
+            {
+                if(pointsDevie > 0)
+                {
+
+                    return true;
+                }
+                return false;
+            }
+
+        }
+    }
+}
