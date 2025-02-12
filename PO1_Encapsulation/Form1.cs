039429022d29027ae70e835b2c82f4149947d5c5
PatrickSugewe <157501471+PatrickSugewe@users.noreply.github.com>
2025-02-12 11:43:39 -0500

    Encapsulation

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,14 +2,33 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+
+        Personnage joueur;
+        Personnage monstre;
+
         public Form1()
         {
             InitializeComponent();
+
+            joueur = new Personnage();
+            joueur.PointDeVie = 10;
+            joueur.PuissanceAttaque = 1;
+            monstre = new Personnage();
+            monstre.PointDeVie = 30;
+            monstre.PuissanceAttaque = 3;
+
         }
+
+
+
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
+            monstre.PointDeVie = monstre.PointDeVie - joueur.PuissanceAttaque;
+            joueur.PointDeVie = joueur.PointDeVie - monstre.PuissanceAttaque;
 
+            lbl_pointsVieDragon.Text = "Points de vie : " + monstre.PointDeVie.ToString();
+            lbl_pointsVieJoueur.Text = "Points de vie : " + joueur.PointDeVie.ToString();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
@@ -18,6 +37,11 @@ namespace PO1_Encapsulation
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        int pointDeVie;
+        int puissanceAttaque;
+
+        public int PointDeVie { get => pointDeVie;
+            set
+            {
+                if (value < 0)
+                {
+                    pointDeVie = 0;
+                }
+                else
+                {
+                    pointDeVie = value;
+                }
+            }
+        }
+        public int PuissanceAttaque { get => puissanceAttaque; set => puissanceAttaque = value; }
+    }
+}
